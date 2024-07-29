640d77773994cd750552d28396986291f1807c70
George Kuegler <georgekuegler@gmail.com>
2024-07-29 07:13:48 -0400

    make auto retry on serial failure

--- arduino.py
+++ arduino.py
@@ -7,13 +7,11 @@ Be very careful.
 import serial
 import json
 import threading
+import time
 
-import http_client
-import control
-import database
-import cmd_shell
 
 class Arduino:
+
     def __init__(self, port, baud_rate, controller):
         self.port = port
         self.baud_rate = baud_rate
@@ -23,24 +21,38 @@ class Arduino:
     def start(self):
         self.thread.start()
 
+    def sample(self, s):
+        # s.flush()
+        try:
+            # Block for arduino to send serial communications.
+            data = json.loads(s.readline())
+            print(data["tempF"])
+            self.controller.update(data["tempF"])
+        except json.JSONDecodeError:
+            print("Message not valid json.")
+            # Tell controller pipelined data is no longer
+            # valid in filters, dsp, etc.
+            self.controller.clear_buf()
+
     def loop(self):
-        with serial.Serial(self.port, self.baud_rate, write_timeout=1, timeout=15) as s:
-            while True:
-                s.flush()
-                try:
-                    # Block for arduino to send serial communications.
-                    msg = s.readline()
-                    # print(f"msg: {msg}")
-
-                    data = json.loads(msg)
-
-                    print(data["tempF"])
-
-                    # Update controller with sensor data.
-                    self.controller.update(data["tempF"])
-
-                except json.JSONDecodeError as ex:
-                    print("Message not valid json.")
-                    # Tell controller pipelined data is no longer
-                    # valid in filters, dsp, etc.
-                    self.controller.clear_buf()
+        while True:
+            try:
+                with serial.Serial(self.port,
+                                   self.baud_rate,
+                                   write_timeout=1,
+                                   timeout=15) as s:
+                    while True:
+                        # Sample rate controlled by arduino.
+                        # This thread blocks until next temp reading available.
+                        self.sample(s)
+            except serial.SerialException as ex:
+                if ex.errno == 2:
+                    print(f"Error Arduino not connected.")
+                else:
+                    print(ex)
+            except Exception as ex:
+                print(ex)
+            finally:
+                self.controller.clear_buf()
+                time.sleep(2)
+                continue
